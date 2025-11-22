a5968e530ed21600c970e1647d146b6068b237c8
suryateja010 <46032294+suryateja010@users.noreply.github.com>
2025-11-22 20:54:07 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World! V1", app.getMessage());
+        assertEquals("Hello World! V2", app.getMessage());
     }
 }
