3374191590d36f4316aaba08311449358e205630
suryateja010 <46032294+suryateja010@users.noreply.github.com>
2025-11-22 21:03:28 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! V3";
+    private static final String MESSAGE = "Hello World! V4";
 
     public App() {}
 
